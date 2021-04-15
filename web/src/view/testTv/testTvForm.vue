cfc4eafa5f3aa328418c4db612afef75d13d39ed
tphyhFighting <2363176358@qq.com>
2021-04-15 23:01:48 +0800

    update: go.mod & package-lock.json & testTvForm.vue

--- web/src/view/testTv/testTvForm.vue
+++ web/src/view/testTv/testTvForm.vue
@@ -58,7 +58,7 @@ export default {
   },
   async created() {
     // 建议通过url传参获取目标数据ID 调用 find方法进行查询数据操作 从而决定本页面是create还是update 以下为id作为url参数示例
-    consoel.log("---->test-")
+    console.log("---->test-")
     if (this.$route.query.id) {
       const res = await findTestTv({ ID: this.$route.query.id });
       if (res.code == 0) {
